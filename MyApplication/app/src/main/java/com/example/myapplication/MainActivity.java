bd2372fefa733a71ce898cf2dd2c79ce3958b33a
animeclash <46046160+animeclash@users.noreply.github.com>
2019-11-05 11:03:57 +0100

    Jean Commit
    Cool

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.fightBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+
+        });
     }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, secondActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
