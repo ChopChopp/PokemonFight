3c7c6fda6ad018fb7386e311a8408ce7eee61814
animeclash <46046160+animeclash@users.noreply.github.com>
2019-11-06 15:48:35 +0100

    remove failure

--- MyApplication/app/src/main/java/com/example/myapplication/secondActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/secondActivity.java
@@ -9,9 +9,7 @@ import android.os.Bundle;
 import com.google.android.material.tabs.TabLayout;
 
 public class secondActivity extends AppCompatActivity {
-
-    MyFragmentPagerAdapter myFragmentPagerAdapter;
-    ViewPager viewPager;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
