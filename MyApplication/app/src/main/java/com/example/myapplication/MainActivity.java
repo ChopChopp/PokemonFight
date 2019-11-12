b14f14efdc09da20a6d3486c69165b75a6701a95
animeclash <46046160+animeclash@users.noreply.github.com>
2019-11-12 09:40:42 +0100

    arararara

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +12,7 @@ import android.widget.Button;
 import com.example.beans.Move;
 import com.orm.SugarContext;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +34,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void goToUsername() {
+
+        List<Move> moves = Move.listAll(Move.class);
+        DAO dao = new DAO();
+
+        if(moves.size() == 0){
+            dao.createMoves();
+            dao.createPokemon();
+            System.out.println("Pokemon & Moves where initialized");
+        }
+        else{
+            System.out.println("Moves & Pokemon are already initialized in the database");
+        }
+
+
+       for(Move m: moves) {
+           System.out.println(m.getName());
+           System.out.println(m.getId());
+       }
 
         Intent intent = new Intent(this, UserName.class);
         startActivity(intent);
