04b7f852a90c9af7c037986fb6d9ecfc3d7af21c
animeclash <46046160+animeclash@users.noreply.github.com>
2019-11-12 13:04:31 +0100

    Umums bizzare adventure

--- MyApplication/app/src/main/java/com/example/myapplication/ChoosePokemon.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ChoosePokemon.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.beans.Pokemon;
@@ -10,6 +13,8 @@ import com.example.beans.Pokemon;
 import java.util.List;
 
 public class ChoosePokemon extends AppCompatActivity {
+
+    boolean clicked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +33,48 @@ public class ChoosePokemon extends AppCompatActivity {
         pokemonName3.setText(pkmnlist.get(2).getName());
         pokemonName4.setText(pkmnlist.get(3).getName());
 
+        ConstraintLayout cl1 = findViewById(R.id.charizardLayout);
+        ConstraintLayout cl2 = findViewById(R.id.greninjaLayout);
+        ConstraintLayout cl3 = findViewById(R.id.sceptileLayout);
+        ConstraintLayout cl4 = findViewById(R.id.luxrayLayout);
+
+        clicked = false;
+
+        cl1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                goToFighting();
+            }
+        });
+        cl2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                goToFighting();
+            }
+        });
+        cl3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                goToFighting();
+            }
+        });
+        cl4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                goToFighting();
+            }
+        });
+
+
+
+    }
+
+    public void goToFighting() {
+        Intent intent = new Intent(this, FightActivity.class);
+        startActivity(intent);
     }
 }
